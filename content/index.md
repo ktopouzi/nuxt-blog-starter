5f48b9ee8f13682d3cc983fc5b45e0b0893a3926
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2024-03-18 13:13:21 +0000

    feat: adds nuxt content

---
+++ content/index.md
@@ -0,0 +1,3 @@
+# hello
+
+this is an md file
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
 	tailwindcss: {
 		editorSupport: true,
+	},
+	content: {
+		highlight: {
+			theme: 'github-dark',
+		},
 	},
 });
